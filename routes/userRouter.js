b945883bf71864f3189f1bb6cab326efc61be98f
vidulanka <vidulankapasindu2001@gmail.com>
2025-06-27 19:12:37 +0530

    Day 20✏️

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { googleLogin, loginUser, saveUser } from "../controllers/userControllers.js";
+import { getCurrentUser, googleLogin, loginUser, saveUser } from "../controllers/userControllers.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", saveUser)
 userRouter.post("/login",loginUser)
 userRouter.post("/google",googleLogin)
+userRouter.get ("/current",getCurrentUser)
 
 
 export default userRouter;
